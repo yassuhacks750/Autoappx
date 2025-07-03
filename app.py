cb113478c22da0934d7d433a4bef10a2fb5a7465
yassuhacks750 <yassudj8085231272@gmail.com>
2025-07-04 02:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World! from spidy"
+@app.route('/')
+def home():
+    return "Web App is running on Render!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
-    
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render द्वारा दिया गया पोर्ट
+    app.run(host="0.0.0.0", port=port)  # सभी नेटवर्क इंटरफेस पर सुनना ज़रूरी है
