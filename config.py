b6c9b2ef2479129e721f11ca4ca0d2273fb54842
yassuhacks750 <yassudj8085231272@gmail.com>
2025-10-05 10:04:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,15 @@ import os
 
 class Config(object):
     BOT_TOKEN = "8434843795:AAFamCgCGaQXXvTDkJ4FWpHnu_Dv4MmKxxE"
-    DB_NAME = "txtbot"
+    DB_NAME = "Yashxug"
     API_ID = "22849789"
     API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
     ADMIN_ID = 7296271316
-    DB_URL = "mongodb+srv://autoappx750:Yash2835P@autoappx.drvhbnu.mongodb.net/?retryWrites=true&w=majority&appName=Autoappx"
+    DB_URL = "mongodb+srv://yashxug:Yash2835P@yashxug.om3yk1q.mongodb.net/retryWrites=true&w=majority&appName=Yashxug"
     LOG_CHANNEL = "-1002477114210" # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
     TUTORIAL_VIDEO = ""
+
 
 
 
